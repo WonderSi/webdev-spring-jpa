805b086d99c11c5919edf64fd542e1861b18b661
WonderSi <kirillkozlov00@gmail.com>
2026-02-23 20:16:25 +0700

    fix: GlobalExceptionHandler import

--- src/main/kotlin/com/example/lab3/web/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/lab3/web/GlobalExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.example.lab3.web
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
